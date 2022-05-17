b32ac80c7c761a29c90cd795d2f2c2cfc484a765
tashteba <91596532+tashteba@users.noreply.github.com>
2022-05-17 16:04:40 +0200

    Add files via upload

---
+++ jvScript/ex.js
@@ -0,0 +1,31 @@
+// EX1 
+//var ss = prompt("Entre Votre Prenom s'il vous plait");
+//function maFonction()
+// {
+//   return "Bonjour M,Mme"+"  "+ ss;
+// }
+
+// // document.getElementById("mon_titre").innerHTML = maFonction();
+
+// Ex2
+//  const n1 = prompt ("Entre numero 1");
+//  const n2 = prompt ("Entre numero 2");
+
+//  function maDeFonction(){
+//      return window.alert(parseInt(n1)+parseInt(n2));
+//  }
+
+//  maDeFonction();
+
+// EX3
+
+var x = prompt("Entre Votre age s'il vous plait");
+
+if ( x > parseInt(18) ){
+    window.alert('You welcome');
+}else {
+    window.alert("Vous pouvez pas entre");
+}
+
+// EX 4
+
